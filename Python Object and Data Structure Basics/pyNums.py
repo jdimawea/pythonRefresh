096f81c40f826eb480bb5a767484b4b3819a6f5c
jdimawea <94670505+jdimawea@users.noreply.github.com>
2024-04-29 23:57:43 -0600

    operations

---
+++ Python Object and Data Structure Basics/pyNums.py
@@ -0,0 +1,28 @@
+# basic operations
+
+def add(x, y):
+    print("Addition:", x + y)
+
+add(2, 3)
+
+def subtract(x, y):
+    print("Subtraction:", x - y)
+
+subtract(10, 3)
+
+def multiply(x, y):
+    print("Multiplication:", x * y)
+
+multiply(2, 5)
+
+def divide(x, y):
+    print("Division:", x / y)
+
+divide(10, 2)
+
+# Modulo or "Mod" Operator (returns the remainder)
+
+def mod(x, y):
+    print("Modulo:", x % y)
+
+mod(7, 4)
