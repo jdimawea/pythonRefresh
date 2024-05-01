35a98405678ed8c37b039ad7e8ba3691f8c56abd
jdimawea <94670505+jdimawea@users.noreply.github.com>
2024-04-30 20:34:05 -0600

    sets

---
+++ Python Object and Data Structure Basics/sets.py
@@ -0,0 +1,10 @@
+my_set = set()
+
+# adding object to the set
+my_set.add(1)
+print(my_set)
+my_set.add(2)
+print(my_set)
+# adding a value that is already in the set won't repeat it. Must be unique
+my_set.add(2)
+print(my_set)
