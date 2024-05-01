10281a8c34bdc5499712eb455cca04e85ddf02b0
jdimawea <94670505+jdimawea@users.noreply.github.com>
2024-04-30 20:26:23 -0600

    tuples

---
+++ Python Object and Data Structure Basics/tuples.py
@@ -0,0 +1,17 @@
+t = (1, 2, 3)
+my_list = [1, 2, 3]
+
+t = ('one', 2)
+print(t[0])
+print(t[-1])
+
+# .count('a') returns how many times a occurs in the tupple
+t = ('a', 'a', 'b')
+print(t.count('a'))
+
+# .index('a') returns when a appears for the first time
+print(t.index('a'))
+
+
+my_list[0] = 'New'
+# t[0] = 'New'
