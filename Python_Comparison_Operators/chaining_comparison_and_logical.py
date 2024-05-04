a4174a35252b0e5c53a80f371af82a36a9bc18db
jdimawea <94670505+jdimawea@users.noreply.github.com>
2024-05-04 14:26:28 -0600

    comparison and logical operators

---
+++ Python_Comparison_Operators/chaining_comparison_and_logical.py
@@ -0,0 +1,11 @@
+# Returns False
+print(1 < 2 and 2 > 3)
+
+# Returns True
+print( ('h' == 'h') and (2 == 2) )
+
+# Returns True since one of them passes
+print((1 == 1) or (100 == 1))
+
+# Returns False, not asks for the opposite boolean
+print(not 1 == 1)
