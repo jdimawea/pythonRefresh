ceabf8eef8d0e426edc514521af7795d3f2f0dae
jdimawea <94670505+jdimawea@users.noreply.github.com>
2024-05-04 14:52:59 -0600

    if elif else

---
+++ python_statements/if_elif_else.py
@@ -0,0 +1,20 @@
+def true_or_false (bool):
+    if bool == True:
+        print('It is True!')
+    elif bool == False:
+        print('It is False!')
+    else:
+        print('Neither True or False')
+
+true_or_false(True)
+true_or_false(False)
+
+def is_hungry (bool):
+    if bool == True:
+        print('Feed me!')
+    elif bool == False:
+        print('I am full')
+    else:
+        print('What is food?')
+
+is_hungry(True)
