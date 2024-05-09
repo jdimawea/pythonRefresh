f2b7f10194c75c000c8d3bb37a9064eacbe31502
jdimawea <94670505+jdimawea@users.noreply.github.com>
2024-05-08 22:06:45 -0600

    while loops

---
+++ python_statements/while_loop.py
@@ -0,0 +1,40 @@
+# x = 0
+
+# while x < 5:
+#     print(f'The current value of x is {x}')
+#     x += 1
+# else:
+#     print('X is not less than 5')
+
+# def x_less_than_5 (num):
+#     while num < 5:
+#         print(f'The current value of the num is {num}')
+#         num += 1
+#     else:
+#         print('Num is not less than 5!')
+
+# x = 2
+# x_less_than_5(x)
+
+
+x = [1, 2, 3]
+
+for item in x:
+    # returns an error if we dont use pass
+    pass
+
+print('end of my script')
+
+
+mystring = "Sammy"
+
+for letter in mystring:
+    if letter == 'a':
+        # continue 
+        break
+    print(letter)
+
+x = 0
+while x < 5:
+    print(x)
+    x += 1
