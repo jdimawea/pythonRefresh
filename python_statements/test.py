e5bbda7bc4deb01f7a3c2f75bab984451e654645
jdimawea <94670505+jdimawea@users.noreply.github.com>
2024-05-10 22:46:12 -0600

    updated

--- python_statements/test.py
+++ python_statements/test.py
@@ -1,13 +1,13 @@
 # Use for, .split() and if to create a Statement that will print out words that start with 's':
-st = 'Print only the words that start with s in this sentence'
+st = 'Sam Print only the words that start with s in this sentence'
 words = st.split()
 for word in words:
-    if word[0] == 's' and len(word) > 1:
+    if word[0].lower() == 's' and len(word) > 1:
         print(word)
 
 
 # Use range() to print all the even numbers from 0 to 10:
-for num in range(0, 11):
+for num in range(0, 11): # or for num in range(0, 10, 2):
     if num % 2 == 0:
         print(num)
 
@@ -22,12 +22,18 @@ st = 'Print every word in this sentence that has an even number of letters'
 words = st.split()
 for word in words:
     if len(word) % 2 == 0:
-        print(word, 'even!')
+        print(word + ' even!')
+
+# Better way
+st = 'Print every word in this sentence that has an even number of letters'
+for word in st.split():
+    if len(word) % 2 == 0:
+        print(word + ' is even!')
 
 
 # Write a program that prints the integers from 1 to 100. But for multiples of three print "Fizz" instead of a number, and for the multiples of five print "Buzz".
 # For numbers which are multiples of both three and five print "FizzBuzz".
-for num in range(0, 101):
+for num in range(1, 101):
     if num % 3 == 0 and num % 5 == 0:
         print('FizzBuzz')
     elif num % 3 == 0:
